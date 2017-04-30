5cfc4e9a862f93a74d84afea461fec6a4a2ffa98
Larry Boateng Asante <boatenga17@grinnell.edu>
2017-04-30 16:42:30 -0500

    Scale the map projection to make space for the timeline scrub

--- final.js
+++ final.js
@@ -2,7 +2,7 @@
 var svg_width = window.innerWidth;
 var svg_height = window.innerHeight;
 
-var projection = d3.geoMercator();
+var projection = d3.geoEquirectangular().translate(500, 500);
 var path = d3.geoPath().projection(projection);
 
 // Generate an SVG element on the page
@@ -10,15 +10,13 @@ var svg = d3.select("body").append("svg")
     .attr("width", svg_width)
     .attr("height", svg_height);
 
-var g = svg.append("g");
-
 d3.json('world-110m.json', function(error, world) {
   // Decode the topojson file
   var land = topojson.feature(world, world.objects.land);
   var countries = topojson.mesh(world, world.objects.countries);
 
   // Fit our projection so it fills the window
-  projection.fitSize([svg_width, svg_height], land);
+  projection.fitSize([svg_width, svg_height-80], land);
 
   // Create land area
   svg.append('path')
@@ -31,14 +29,4 @@ d3.json('world-110m.json', function(error, world) {
     .datum(countries)
     .attr('class', 'state-boundary')
     .attr('d', path);
-
-  g.append("g")
-     .attr("id", "countries")
-     .selectAll("path")
-     .data(topojson.feature(world, word.objects.countries).features)
-     .enter()
-     .append("path")
-     .attr("id", function(d) { return d.id; })
-     .attr("d", path)
-     .on("click", country_clicked);
 });
