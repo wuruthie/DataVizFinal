55618d2813124982a7858d3a145db3986613859b
Larry Boateng Asante <boatenga17@grinnell.edu>
2017-05-10 23:48:35 -0500

    Clean up code and fix bug

--- final.js
+++ final.js
@@ -129,10 +129,11 @@ d3.queue()
         eventsByYear.filter([1990, 1991]); // Using some dummy years. Change the min and max date per the slider direction. If forward, add one; if backwards, subtract one
 
         // Prints out all the events that happened between 1990 and 1991
-        //    console.log(eventsByYear.top(Infinity));
 
         var data = eventsByYear.top(Infinity);
-        visualizeData(data);
+
+        var color_arr = ['#33691E']
+        visualizeData(data, color_arr, 1990); // Use a default color and start from the first year in the dataset
     });
 
 // Credit for base implementation of slider to: https://bl.ocks.org/mbostock/6499018
@@ -190,6 +191,7 @@ var handle = slider.insert("circle", ".track-overlay")
 
 
 function displayPointsByYear(year) {
+
     handle.attr("cx", x(year));
 
     var colors = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6"];
